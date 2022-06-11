6290443b7f7b536e45d59a8da2c1a72bb854dfd7
Alex Spaeth <atspaeth@ucsc.edu>
2022-06-10 19:04:08 -0700

    Switch to node coordinates WITHOUT parity check.

--- POM/pom.py
+++ POM/pom.py
@@ -13,10 +13,16 @@ A = np.array([
 
 
 class Node:
-    def __init__(self, x, y, z, nid):
-        assert nid in range(6)
+    def __init__(self, x, y, z, nid=None):
         self.pos = np.array([x, y, z])
-        self.id = nid
+        assert (self.pos % 2).sum() == 2
+
+        # The user can specify their own node ID, but it is checked
+        # for correctness.
+        (self.nid,) = np.nonzero(self.pos % 2 == 0)[0]
+        if nid is not None:
+            assert nid in range(6) and nid % 3 == self.nid
+            self.nid = nid
 
 class Servo:
     def __init__(self, node, direction):
@@ -32,12 +38,12 @@ class Servo:
         # will raise a ValueError here).
         planes = np.ones(3, bool)
         planes[direction] = False
-        planes[self.node.id % 3] = False
+        planes[self.node.nid % 3] = False
         (self.pom,) = np.nonzero(planes)[0]
 
         # Correction to the sign for when this servo rotates the plane
         # of motion "backwards".
-        self._c_id = A[:, direction, self.node.id].sum()
+        self._c_id = A[:, direction, self.node.nid].sum()
 
     def conflicts_with(self, other):
         'Return whether this servo shares a plane of motion with another.'
@@ -50,8 +56,7 @@ class Servo:
         actuated by a given amount.
         '''
         if node.pos[self.pom] == self.node.pos[self.pom]:
-            c_pos = (-1)**abs((self.node.pos - node.pos).sum())
-            return self._c_id * c_pos * amount * A[self.pom, :, node.id]
+            return self._c_id * amount * A[self.pom, :, node.nid]
         return np.zeros(3)
 
 
@@ -60,15 +65,15 @@ class Voxels:
         self.servos = []
         self.effectors = []
 
-    def add_servo(self, x, y, z, nid, direction):
-        new = Servo(Node(x, y, z, nid), direction)
+    def add_servo(self, node, direction):
+        new = Servo(node, direction)
         for servo in self.servos:
             if servo.conflicts_with(new):
                 raise ValueError('Servo makes system overdetermined.')
         self.servos.append(new)
 
-    def add_effector(self, x, y, z, nid):
-        self.effectors.append(Node(x, y, z, nid))
+    def add_effector(self, node):
+        self.effectors.append(node)
 
     def actuate(self, *amounts):
         assert len(amounts) == len(self.servos)
@@ -83,115 +88,84 @@ class Voxels:
 
 
 class TestNodeInvariants:
-    def test_node_ids_must_be_0_to_5(self):
+    def test_nodes_must_lie_on_grid(self):
         from pytest import raises
         with raises(AssertionError):
-            Node(1, 2, 3, 6)
+            Node(0,0,0)
+
+    def test_nodes_from_later_tests_are_ok(self):
+        Node(2, 1, 1)
+        Node(0, 1, 3)
+        Node(1, 1, 4)
+
+    def test_node_ids_can_be_inferred(self):
+        assert Node(0, 1, 3).nid == 0
+        assert Node(1, 1, 4).nid == 2
+
+    def test_node_ids_can_be_provided(self):
+        Node(0, 1, 3, 3)
+        Node(1, 1, 4, 5)
+
+    def test_bad_node_ids_are_detected(self):
+        from pytest import raises
+        with raises(AssertionError):
+            Node(0, 1, 3, 2)
+        with raises(AssertionError):
+            Node(1, 1, 4, 3)
 
 class TestServoInvariants:
     class TestInitializationValidation:
         def test_nodes_cant_move_in_their_fixed_direction(self):
             from pytest import raises
             with raises(ValueError):
-                Servo(Node(0,0,0,0), 0)
+                Servo(Node(0,1,1,0), 0)
 
             with raises(ValueError):
-                Servo(Node(0,0,0,1), 1)
+                Servo(Node(1,0,1,1), 1)
 
             with raises(ValueError):
-                Servo(Node(0,0,0,2), 2)
+                Servo(Node(1,1,0,2), 2)
 
-            with raises(ValueError):
-                Servo(Node(0,0,0,3), 0)
-
-            with raises(ValueError):
-                Servo(Node(0,0,0,4), 1)
-
-            with raises(ValueError):
-                Servo(Node(0,0,0,5), 2)
 
     class TestBaseNodeMovesByActuationAmount:
-
         def test_node_1_X(self):
             v = Voxels()
-            v.add_servo(0, 0, 0, 1, 0)
-            v.add_effector(0, 0, 0, 1)
-            v.add_effector(0, -1, 0, 4)
-            assert np.all(v.actuate(5) == [5, 0, 0])
-
-        def test_node_5_X(self):
-            v = Voxels()
-            v.add_servo(0, 0, 0, 5, 0)
-            v.add_effector(0, 0, 0, 5)
-            v.add_effector(0, 0, 1, 2)
-            assert np.all(v.actuate(5) == [5, 0, 0])
-
-        def test_node_4_X(self):
-            v = Voxels()
-            v.add_servo(0, -1, 0, 4, 0)
-            v.add_effector(0, 0, 0, 1)
-            v.add_effector(0, -1, 0, 4)
+            n = Node(1, 0, 1)
+            v.add_servo(n, 0)
+            v.add_effector(n)
             assert np.all(v.actuate(5) == [5, 0, 0])
 
         def test_node_2_X(self):
             v = Voxels()
-            v.add_servo(0, 0, 1, 2, 0)
-            v.add_effector(0, 0, 0, 5)
-            v.add_effector(0, 0, 1, 2)
+            n = Node(1, 1, 0)
+            v.add_servo(n, 0)
+            v.add_effector(n)
             assert np.all(v.actuate(5) == [5, 0, 0])
-
-        def test_node_2_Y(self):
-            v = Voxels()
-            v.add_servo(0, 0, 0, 2, 1)
-            v.add_effector(0, 0, 0, 2)
-            v.add_effector(0, 0, -1, 5)
-            assert np.all(v.actuate(5) == [0, 5, 0])
-
-        def test_node_5_Y(self):
-            v = Voxels()
-            v.add_servo(0, 0, -1, 5, 1)
-            v.add_effector(0, 0, 0, 2)
-            v.add_effector(0, 0, -1, 5)
-            assert np.all(v.actuate(5) == [0, 5, 0])
-
-        def test_node_3_Y(self):
-            v = Voxels()
-            v.add_servo(0, 0, 0, 3, 1)
-            v.add_effector(0, 0, 0, 3)
-            v.add_effector(1, 0, 0, 0)
-            assert np.all(v.actuate(5) == [0, 5, 0])
 
         def test_node_0_Y(self):
             v = Voxels()
-            v.add_servo(1, 0, 0, 0, 1)
-            v.add_effector(0, 0, 0, 3)
-            v.add_effector(1, 0, 0, 0)
+            n = Node(0, 1, 1)
+            v.add_servo(n, 1)
+            v.add_effector(n)
+            assert np.all(v.actuate(5) == [0, 5, 0])
+
+        def test_node_2_Y(self):
+            v = Voxels()
+            n = Node(1, 1, 0)
+            v.add_servo(n, 1)
+            v.add_effector(n)
             assert np.all(v.actuate(5) == [0, 5, 0])
 
         def test_node_0_Z(self):
             v = Voxels()
-            v.add_servo(0, 0, 0, 0, 2)
-            v.add_effector(0, 0, 0, 0)
-            v.add_effector(-1, 0, 0, 3)
-            assert np.all(v.actuate(5) == [0, 0, 5])
-
-        def test_node_3_Z(self):
-            v = Voxels()
-            v.add_servo(-1, 0, 0, 3, 2)
-            v.add_effector(0, 0, 0, 0)
-            v.add_effector(-1, 0, 0, 3)
-            assert np.all(v.actuate(5) == [0, 0, 5])
-
-        def test_node_4_Z(self):
-            v = Voxels()
-            v.add_servo(0, 0, 0, 4, 2)
-            v.add_effector(0, 0, 0, 4)
-            v.add_effector(0, 1, 0, 1)
+            n = Node(0, 1, 1)
+            v.add_servo(n, 2)
+            v.add_effector(n)
             assert np.all(v.actuate(5) == [0, 0, 5])
 
         def test_node_1_Z(self):
             v = Voxels()
-            v.add_servo(0, 1, 0, 1, 2)
-            v.add_effector(0, 0, 0, 4)
-            v.add_effector(0, 1, 0, 1)
+            n = Node(1, 0, 1)
+            v.add_servo(n, 2)
+            v.add_effector(n)
             assert np.all(v.actuate(5) == [0, 0, 5])
