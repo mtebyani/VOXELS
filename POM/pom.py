46ea9aec53a1e0cf4e6cfecef120ec9bf8bc97a5
Alex Spaeth <atspaeth@ucsc.edu>
2022-06-10 18:43:17 -0700

    Make servos have just one POM.

--- POM/pom.py
+++ POM/pom.py
@@ -18,50 +18,41 @@ class Node:
         self.pos = np.array([x, y, z])
         self.id = nid
 
-    def position(self):
-        'Return the absolute position of the node.'
-        axis_offset = 0.5*np.ones(3)
-        axis_offset[self.id % 3] = self.id // 3
-        return axis_offset + self.pos
-
-    def planes_match(self, other):
-        'Return whether each of the three planes matches.'
-        return self.position() == other.position()
-
 class Servo:
     def __init__(self, node, direction):
         assert direction in range(3)
 
         self.node = node
-        self._planes = np.arange(3) != direction
+
+        # Unfortunately we can only identify the correct plane of
+        # motion by elimination: it can't be the plane where the
+        # actuator moves or the plane where it's on a voxel boundary.
+        # This is always unique because it's physically impossible to
+        # rotate the actuator to make those two planes the same (this
+        # will raise a ValueError here).
+        planes = np.ones(3, bool)
+        planes[direction] = False
+        planes[self.node.id % 3] = False
+        (self.pom,) = np.nonzero(planes)[0]
 
         # Correction to the sign for when this servo rotates the plane
         # of motion "backwards".
         self._c_id = A[:, direction, self.node.id].sum()
 
-        if node.id % 3 == direction:
-            DL = 'XYZ'[direction]
-            raise ValueError(f'Servo at node {node} cannot actuate in {DL}')
-
     def conflicts_with(self, other):
         'Return whether this servo shares a plane of motion with another.'
-        return np.any(self._planes & other._planes
-                      & self.node.planes_match(other.node))
-
-    def shared_planes(self, node):
-        'Return the planes this Servo shares with some voxel position.'
-        return np.nonzero(self.node.planes_match(node) & self._planes)[0]
+        return (self.pom == other.pom
+                and self.node.pos[self.pom] == other.node.pos[other.pom])
 
     def actuate(self, node, amount):
         '''
-        Return how much this servo affects the specified node when actuated by
-        a given amount.
+        Return how much this servo affects the specified node when
+        actuated by a given amount.
         '''
-        ret = np.zeros(3)
-        for p in self.shared_planes(node):
+        if node.pos[self.pom] == self.node.pos[self.pom]:
             c_pos = (-1)**abs((self.node.pos - node.pos).sum())
-            ret += self._c_id * c_pos * amount * A[p, :, node.id]
-        return ret
+            return self._c_id * c_pos * amount * A[self.pom, :, node.id]
+        return np.zeros(3)
 
 
 class Voxels:
@@ -85,7 +76,7 @@ class Voxels:
         disp = []
         for e,effector in enumerate(self.effectors):
             disp_e = np.zeros(3)
-            for amount,(s,servo) in zip(amounts, enumerate(self.servos)):
+            for amount,servo in zip(amounts, self.servos):
                 disp_e += servo.actuate(effector, amount)
             disp.append(disp_e)
         return np.array(disp)
@@ -97,84 +88,28 @@ class TestNodeInvariants:
         with raises(AssertionError):
             Node(1, 2, 3, 6)
 
-    def test_position_offsets_dont_depend_on_lattice_position(self):
-        pos = np.random.randint(10, size=3)
-
-        n = Node(*pos, 0)
-        assert np.all(n.position() == pos+[0., 0.5, 0.5])
-
-        n = Node(*pos, 1)
-        assert np.all(n.position() == pos+[0.5, 0., 0.5])
-
-        n = Node(*pos, 2)
-        assert np.all(n.position() == pos+[0.5, 0.5, 0.])
-
-        n = Node(*pos, 3)
-        assert np.all(n.position() == pos+[1., 0.5, 0.5])
-
-        n = Node(*pos, 4)
-        assert np.all(n.position() == pos+[0.5, 1., 0.5])
-
-        n = Node(*pos, 5)
-        assert np.all(n.position() == pos+[0.5, 0.5, 1.])
-
-    class TestSharedPlanesForSameID:
-        def test_all_coords_different_means_no_planes_shared(self):
-            for _ in range(100):
-                nid = np.random.randint(6)
-                pos = np.random.randint(10, size=3)
-                offset = 1 + np.random.randint(8, size=3)
-                offset[offset > 4] -= 9
-                n1, n2 = Node(*pos, nid), Node(*pos+offset, nid)
-                assert not np.any(n1.planes_match(n2))
-
-        def test_one_coord_same_means_that_plane_shared(self):
-            for _ in range(100):
-                shared_plane = np.random.randint(3)
-                nid = np.random.randint(6)
-                pos = np.random.randint(10, size=3)
-                offset = 1 + np.random.randint(8, size=3)
-                offset[offset > 4] -= 9
-                offset[shared_plane] = 0
-                n1, n2 = Node(*pos, nid), Node(*pos+offset, nid)
-                expected = np.arange(3) == shared_plane
-                assert np.all(n1.planes_match(n2) == expected)
-
-    class TestSharedPlanesForCompatibleID:
-        def test_one_coord_offset_correctly_means_that_plane_shared(self):
-            for _ in range(100):
-                nid1 = np.random.randint(6)
-                nid2 = nid1-3 if nid1 >= 3 else nid1+3
-                shared_plane = nid1 % 3
-                pos = np.random.randint(10, size=3)
-                offset = 1 + np.random.randint(8, size=3)
-                offset[offset > 4] -= 9
-                offset[shared_plane] = 1 if nid1 >= 3 else -1
-                n1, n2 = Node(*pos, nid1), Node(*pos+offset, nid2)
-                expected = np.arange(3) == shared_plane
-                assert np.all(n1.planes_match(n2) == expected)
-
-        def test_one_coord_offset_wrong_means_no_planes_shared(self):
-            for _ in range(100):
-                nid1 = np.random.randint(6)
-                nid2 = nid1-3 if nid1 >= 3 else nid1+3
-                shared_plane = nid1 % 3
-                pos = np.random.randint(10, size=3)
-                offset = 1 + np.random.randint(8, size=3)
-                offset[offset > 4] -= 9
-                offset[shared_plane] = 42
-                n1, n2 = Node(*pos, nid1), Node(*pos+offset, nid2)
-                assert not np.any(n1.planes_match(n2))
-
-    class TestSharedPlanesForIncompatibleID:
-        def test_same_voxel_incompatible_node_means_plane_not_shared(self):
-            n1, n2 = Node(0,0,0,1), Node(0,0,0,2)
-            assert np.all(n1.planes_match(n2) == [True, False, False])
-
-
-
-
 class TestServoInvariants:
+    class TestInitializationValidation:
+        def test_nodes_cant_move_in_their_fixed_direction(self):
+            from pytest import raises
+            with raises(ValueError):
+                Servo(Node(0,0,0,0), 0)
+
+            with raises(ValueError):
+                Servo(Node(0,0,0,1), 1)
+
+            with raises(ValueError):
+                Servo(Node(0,0,0,2), 2)
+
+            with raises(ValueError):
+                Servo(Node(0,0,0,3), 0)
+
+            with raises(ValueError):
+                Servo(Node(0,0,0,4), 1)
+
+            with raises(ValueError):
+                Servo(Node(0,0,0,5), 2)
+
     class TestBaseNodeMovesByActuationAmount:
 
         def test_node_1_X(self):
